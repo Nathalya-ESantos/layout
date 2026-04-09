3dc75be3b420ecae0caf233cdd5bf8acd042216b
nathalya <nathalya.e.santos@aluno.senai.br>
2026-04-09 11:08:35 -0300

    iniciando

---
+++ script.js
@@ -0,0 +1,29 @@
+const colorPicker = document.querySelector('#color-picker');
+const Fontselect = document.querySelector('#font-select');
+const themeToggle = document.querySelector('#theme-toggle');
+const articleColorPicker= document.querySelector('#article-color-picker');
+const body = document.querySelector('#body');
+const newArticles = document.querySelector('#.news');
+
+
+const loadPreferences = () => {
+    // cor
+    const savedColor = localStorage.getItem('primaryColor');
+
+}
+
+colorPicker.addEventListener('input', (e) => {
+
+});
+
+articleColorPicker.addEventListener('input', (e) => {
+
+});
+
+Fontselect.addEventListener('change', (e) => {
+
+});
+
+themeToggle.addEventListener('click', () => {
+
+});
