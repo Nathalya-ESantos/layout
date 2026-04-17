61ff3528869b2115e9cd29a8db4b568be8da45be
nathalya <nathalya.e.santos@aluno.senai.br>
2026-04-17 15:27:29 -0300

    fazendo alterações

---
+++ menu-responsivo-mobile-main/script.js
@@ -0,0 +1,16 @@
+const criarMenu = (menu) => {
+    const botao = menu.querySelector('.menu__botao');
+    const toggleMenu = () => {
+
+    }
+};
+
+const fecharComEsc = (event) =>{
+
+};
+
+
+
+
+document.querySelectorAll('[data-menu]').forEach((menu) => criarMenu(menu));
+
